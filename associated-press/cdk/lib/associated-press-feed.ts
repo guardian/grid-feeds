d66308e625646fd905b78030c3072fbaf88f0732
Pete F <pete.faulconbridge@theguardian.com>
2025-07-16 19:21:24 +0100

    CDK upgrade step 1 -- detach Wazuh SG (see https://github.com/guardian/cdk/blob/df676ec08665bca7f6f689d399aada27981c12e2/CHANGELOG.md\?plain\=1\#L111)

--- associated-press/cdk/lib/associated-press-feed.ts
+++ associated-press/cdk/lib/associated-press-feed.ts
@@ -5,7 +5,12 @@ import { GuAllowPolicy } from '@guardian/cdk/lib/constructs/iam';
 import type { App } from 'aws-cdk-lib';
 import { Fn, Tags } from 'aws-cdk-lib';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { InstanceClass, InstanceSize, InstanceType } from 'aws-cdk-lib/aws-ec2';
+import {
+	InstanceClass,
+	InstanceSize,
+	InstanceType,
+	SecurityGroup,
+} from 'aws-cdk-lib/aws-ec2';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 
@@ -51,7 +56,7 @@ export class AssociatedPressFeed extends GuStack {
 			}),
 		];
 
-		new GuPlayWorkerApp(this, {
+		const ec2App = new GuPlayWorkerApp(this, {
 			app: props.app ?? 'associated-press-feed',
 			instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.SMALL),
 			monitoringConfiguration: {
@@ -78,6 +83,22 @@ export class AssociatedPressFeed extends GuStack {
 			imageRecipe: {
 				Recipe: 'editorial-tools-jammy-java17',
 			},
+			instanceMetricGranularity: stage === 'PROD' ? '1Minute' : '5Minute',
+		});
+
+		const { vpc } = ec2App;
+
+		// A temporary security group with a fixed logical ID, replicating the one removed from GuCDK v61.5.0.
+		const tempSecurityGroup = new SecurityGroup(this, 'WazuhSecurityGroup', {
+			vpc,
+			// Must keep the same description, else CloudFormation will try to replace the security group
+			// See https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ec2-securitygroup.html#cfn-ec2-securitygroup-groupdescription.
+			description: 'Allow outbound traffic from wazuh agent to manager',
+		});
+		this.overrideLogicalId(tempSecurityGroup, {
+			logicalId: 'WazuhSecurityGroup',
+			reason:
+				"Part one of updating to GuCDK 61.5.0+ whilst using Riff-Raff's ASG deployment type",
 		});
 	}
 }
